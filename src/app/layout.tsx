faeaa122707c65d1a79f51e59155ab8d1b7dfe59
Abhishek <abhishek@Abhisheks-MacBook-Air-2.local>
2026-09-09 21:28:13 +0530

    Use raw HTML script tag for AdSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7241227459691122"
-          crossOrigin="anonymous"
-          strategy="lazyOnload"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7241227459691122" crossOrigin="anonymous"></script>
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col transition-colors duration-300`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
