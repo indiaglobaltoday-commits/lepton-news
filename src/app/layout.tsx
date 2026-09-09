0d4c2c08bab8ab7429f4031f73716207423ea304
Abhishek <abhishek@Abhisheks-MacBook-Air-2.local>
2026-09-09 20:54:40 +0530

    Update AdSense Publisher ID to new account

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7542773116944568"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7241227459691122"
           crossOrigin="anonymous"
           strategy="lazyOnload"
         />
