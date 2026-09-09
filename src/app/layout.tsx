980849e2e85bb58a210ef2d409667f4df09239c3
Abhishek <abhishek@Abhisheks-MacBook-Air-2.local>
2026-09-09 20:43:19 +0530

    Update AdSense Publisher ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7542773116944568"
           crossOrigin="anonymous"
           strategy="lazyOnload"
         />
